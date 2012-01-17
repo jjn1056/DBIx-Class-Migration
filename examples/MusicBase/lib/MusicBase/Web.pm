b077f4527fce3efb96d5e64fc83af02c5f955ccd
John Napiorkowski <jjn1056@yahoo.com>
2012-01-17 11:33:46 -0500

    safer probably

--- examples/MusicBase/lib/MusicBase/Web.pm
+++ examples/MusicBase/lib/MusicBase/Web.pm
@@ -1,11 +1,11 @@
 package MusicBase::Web;
 
 use Moose;
+use Catalyst;
 extends 'Catalyst';
 
 our $VERSION = '0.01';
 
-__PACKAGE__->setup_home;
 __PACKAGE__->config(
   'Plugin::ConfigLoader' => {
     file => __PACKAGE__->path_to('share', 'etc'),
