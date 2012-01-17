4a50e1479a7655af7729653c523cb9937b95e354
John Napiorkowski <jjn1056@yahoo.com>
2012-01-17 11:25:00 -0500

    tweaked catalyst example;

--- examples/MusicBase/lib/MusicBase/Web.pm
+++ examples/MusicBase/lib/MusicBase/Web.pm
@@ -1,19 +1,16 @@
 package MusicBase::Web;
 
 use Moose;
-use Catalyst qw/
-  ConfigLoader
-/;
-
 extends 'Catalyst';
 
 our $VERSION = '0.01';
 
+__PACKAGE__->setup_home;
 __PACKAGE__->config(
   'Plugin::ConfigLoader' => {
     file => __PACKAGE__->path_to('share', 'etc'),
   },
 );
 
-__PACKAGE__->setup;
+__PACKAGE__->setup(qw/ConfigLoader/);
 __PACKAGE__->meta->make_immutable;
