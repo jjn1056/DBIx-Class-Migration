ae3d935ede47f91f320fc108dd68de918c0f6e60
John Napiorkowski <jjn1056@yahoo.com>
2012-01-27 09:33:32 -0500

    cleanup schema loader a bit

--- lib/DBIx/Class/Migration/SchemaLoader.pm
+++ lib/DBIx/Class/Migration/SchemaLoader.pm
@@ -6,30 +6,37 @@ use DBIx::Class::Schema::Loader;
 
 has schema => (is=>'ro', required=>1);
 
-my %opts = (
+sub opts {
   naming => { ALL => 'v7'},
   use_namespaces => 1,
-  debug => $ENV{DBIC_MIGRATION_DEBUG}||0);
+  debug => ($ENV{DBIC_MIGRATION_DEBUG}||0);
+}
 
-my $cnt = 0;
+sub merge_opts { opts(), @_ };
+
+sub _make_schema_at {
+  my ($self, $name, %extra_opts) = @_;
+  my $schema = $self->schema->clone;
+  DBIx::Class::Schema::Loader::make_schema_at
+    $name, {merge_opts(%extra_opts)}, [ sub {$schema->storage->dbh} ];
+}
+
+  my $_cnt = 0;
+  sub next_cnt { $_cnt++ }
+  sub _as_unique_ns { shift . next_cnt() }
 
 sub schema_from_database {
-  my $schema = shift->schema->clone;
-  my $name = shift . $cnt++;
-  DBIx::Class::Schema::Loader::make_schema_at
-    $name, \%opts, [ sub {$schema->storage->dbh} ];
+  my ($self, $ns) = @_;
+  $self->_make_schema_at(_as_unique_ns($ns));
 }
 
 sub generate_dump {
-  my ($self, $name, $dump_dir) = @_;
-  my $schema = $self->schema->clone;
-  my %local_opts = (
-    %opts,
+  my ($self, $ns, $dump_dir) = @_;
+  $self->_make_schema_at(
+    $ns,
     dump_directory => $dump_dir,
-    exclude => qr/dbix_class_deploymenthandler_versions/);
-
-  DBIx::Class::Schema::Loader::make_schema_at
-    $name, \%local_opts, [ sub {$schema->storage->dbh} ];
+    exclude => qr/dbix_class_deploymenthandler_versions/,
+  );
 }
 
 1;
