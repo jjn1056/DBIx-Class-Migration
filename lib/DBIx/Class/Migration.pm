348286446ff1cb8fcaacc32919f74687f58028da
Eugen Konkov <kes-kes@yandex.ru>
2018-07-18 15:04:16 +0300

    We dumping our result sets not underlied database
    FIXES #101

--- lib/DBIx/Class/Migration.pm
+++ lib/DBIx/Class/Migration.pm
@@ -446,7 +446,7 @@ sub dump_all_sets {
   (my $self = shift)->dbic_dh->version_storage_is_installed
     || print "Target DB is not versioned.  Dump may not be reliable.\n";
 
-  my $schema = $self->_schema_from_database;
+  my $schema = $self->schema->clone;
 
   $self->build_dbic_fixtures->dump_all_config_sets({
     schema => $schema,
